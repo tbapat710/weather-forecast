e59c768c4b5d88bbf168b3da3cc855a5d5c2bcd2
tarunbapat710 <tbapat@deqode.com>
2022-05-31 19:49:57 +0530

    updated forecast data

--- src/utils/ForecastData.js
+++ src/utils/ForecastData.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ForecastData = async (city) => {
   const resp = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=bf34c95c549370af5a47e8d9ab56bda6`
+    `${process.env.REACT_APP_WEATHER_DOMAIN}?q=${city}&appid=bf34c95c549370af5a47e8d9ab56bda6`
   );
   const data = await resp.json();
   return data;
